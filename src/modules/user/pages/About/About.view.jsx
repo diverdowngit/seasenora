2c170f0fe4481f59dc2cda2124234df36b9f9418
Fred <73175901+diverdowngit@users.noreply.github.com>
2023-01-07 13:44:01 +0100

    added footer

--- src/modules/user/pages/About/About.view.jsx
+++ src/modules/user/pages/About/About.view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from '../../components/Footer/Footer';
 
 import Layout from '../../components/Layout/Layout';
 import Hero from '../Home/components/Hero/Hero';
@@ -9,6 +10,7 @@ export default function AboutView() {
     <Layout>
        <Hero img="/img/aboutTravel1.jpg" centerText="Our Passion For Travel Is The Foundation Of Sv Sea Senora." subheading="Our Traveles  Take You With Us Off-the-beaten-path To Hidden Gems." location="About Us Page "/>
       <AboutSection />
+      <Footer />
     </Layout>
   );
 }
