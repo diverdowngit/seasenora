b38b33c921415ed8d910d5b83dd4f059cf99df62
Fred <73175901+diverdowngit@users.noreply.github.com>
2023-01-12 10:50:15 +0100

    changed home and about

--- src/modules/user/pages/Blog/Blog.view.jsx
+++ src/modules/user/pages/Blog/Blog.view.jsx
@@ -6,21 +6,21 @@ import Hero from "../Home/components/Hero/Hero";
 import BlogList from "./components/BlogList/BlogList";
 
 export default function BlogView() {
-   const [blogList, setBlogList] = React.useState([]);
+  //  const [blogList, setBlogList] = React.useState([]);
 
-  // React.useEffect(() => {
-  //   getBlogItems()
-  // }, []);
+  //   React.useEffect(() => {
+  //    getBlogItems()
+  //   }, []);
 
-  // async function getBlogItems() {
-  //   try {
-  //     const response = await fetch(`${API_URL}/blogs`);
-  //     const data = await response.json();
+  //   async function getBlogItems() {
+  //     try {
+  //       const response = await fetch(`${API_URL}/blogs`);
+  //       const data = await response.json();
   //     setBlogList(data.blogs);
-  //   } catch (error) {
-  //     console.log({ error });
+  //     } catch (error) {
+  //       console.log({ error });
+  //    }
   //   }
-  // }
   return (
     <Layout>
       <Hero img="/img/train.jpg" centerText="Sv Sea Senora, " subheading="We Write Travel Blogs On The Most Stunning Locations On Earth." location="Glenfinnan Viaduct, Glenfinnan, United Kingdom"/>
--- src/modules/user/pages/Home/components/PhotoGallery/PhotoGallery.jsx
+++ src/modules/user/pages/Home/components/PhotoGallery/PhotoGallery.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import styles from './PhotoGallery.module.css';
 import classnames from 'classnames';
-
-import Navbar from "../../../../components/Navbar/Navbar";
 import Layout from "../../../../components/Layout/Layout";
 export default function PhotoGallery() {
     return (  
@@ -10,20 +8,201 @@ export default function PhotoGallery() {
      <div className={styles.container}>
         <div className={styles.wrapper}>
           <h2 className={styles.headingGrid}>Photo Gallery</h2>
-          <div className={styles.containerimg}>
-            <div className={styles.gallery}>
-              <div className={classnames(styles.imggrid1, styles.singlecolumn)}><img className={styles.galleryimg} src="img/gridimg1.jpg" alt="" /><p className={styles.gridText}>Wadi Musa, Ma'an Governorate, Jordan</p></div>
-              <div className={classnames(styles.imggrid2, styles.singlecolumn)}><img className={styles.galleryimg} src="img/gridimg2.jpg" alt="" /><p className={styles.gridText}>Cliffside Village</p></div>
-              <div className={styles.imggrid3}><img className={styles.galleryimg} src="img/gridimg3.jpg" alt="" /><p className={styles.gridText}>Breathtaking seascape with amazing coastal village on cliff</p></div>
-              <div className={styles.imggrid4}><img className={styles.galleryimg} src="img/gridimg4.jpg" alt="" /><p className={styles.gridText}> Buildings and Canal during Night time</p></div>
-              <div className={styles.imggrid5}><img className={styles.galleryimg} src="img/gridimg5.jpg" alt="" /><p className={styles.gridText}>Riva del Garda, Trentino-Alto Adige, Italy</p></div>
-              <div className={styles.imggrid6}><img className={styles.galleryimg} src="img/gridimg6.jpg" alt="" /><p className={styles.gridText}>Bali, Indonesia</p></div>
-            </div>
-          </div>
+          <div className={styles.galleryimage}>
+          <div className={styles.imgbox}>
+      <img src="img/KastelSucuracCroatia.jpg" alt="Kastel Sucurac Croatia" />
+      <div className={styles.transparentbox}>
+        <div className={styles.caption}>
+          <p>Kastel Sucurac Croatia</p>
+          
         </div>
-        {/* <div className={styles.doodle}><img src="https://pixabay.com/de/photos/embudu-insel-urlaub-meer-ozean-4026462/"></img></div>  */}
+      </div> 
+    </div>
+
+    <div className={styles.imgbox}>
+      <img src="img/MilnaMarina.jpg" alt="MilnaMarina.jpg" />
+      <div className={styles.transparentbox}>
+        <div className={styles.caption}>
+          <p>Milna Marina</p>
+          
+        </div>
+      </div> 
+    </div><div className={styles.imgbox}>
+      <img src="img/MaslinicaCroatia.jpg" alt="MaslinicaCroatia.jpg" />
+      <div className={styles.transparentbox}>
+        <div className={styles.caption}>
+          <p>Maslinica Croatia  on a Mooring Ball</p>
+         
+        </div>
+      </div> 
+    </div>
+    <div className={styles.imgbox}>
+      <img src="img/MilnaCroatia.jpeg" alt="MilnaCroatia.jpeg" />
+      <div className={styles.transparentbox}>
+        <div className={styles.caption}>
+          <p>MilnaCroatia</p>
+        </div>
+      </div> 
+    </div>
+    <div className={styles.imgbox}>
+      <img src="img/KastelGomilicaCroatia.jpg" alt="img/KastelGomilicaCroatia" />
+      <div className={styles.transparentbox}>
+        <div className={styles.caption}>
+          <p>Kastel Gomilica Croatia</p>
+          <p className={styles.opacitylow}>Beach</p>
+        </div>
+      </div> 
+    </div>
+    <div className={styles.imgbox}>
+      <img src="img/StomorskaCroatia.jpg" alt="img/StomorskaCroatia.jpg" />
+      <div className={styles.transparentbox}>
+        <div className={styles.caption}>
+          <p>Stomorska Croatia</p>
+          
+        </div>
+      </div> 
+    </div>
+    <div className={styles.imgbox}>
+    <video id="background-video" loop autoPlay muted>
+        <source src="img/windysail.mp4" type="video/mp4" />
+</video>
+      <div className={styles.transparentbox}>
+        <div className={styles.caption}>
+          <p>Adriatic Sea</p>
+         
+        </div>
+      </div>  
+    </div>
+    
+    
+    
+    
+    
+    
+    {/* <div className={styles.imgbox}>
+      <img src="https://picsum.photos/350/250?image=444" alt="" />
+      <div className={styles.transparentbox}>
+        <div className={styles.caption}>
+          <p>Library</p>
+          <p className={styles.opacitylow}>Architect Design</p>
+        </div>
+      </div> 
+    </div><div className={styles.imgbox}>
+      <img src="https://picsum.photos/350/250?image=444" alt="" />
+      <div className={styles.transparentbox}>
+        <div className={styles.caption}>
+          <p>Library</p>
+          <p className={styles.opacitylow}>Architect Design</p>
+        </div>
+      </div> 
+    </div><div className={styles.imgbox}>
+      <img src="https://picsum.photos/350/250?image=444" alt="" />
+      <div className={styles.transparentbox}>
+        <div className={styles.caption}>
+          <p>Library</p>
+          <p className={styles.opacitylow}>Architect Design</p>
+        </div>
+      </div> 
+    </div><div className={styles.imgbox}>
+      <img src="https://picsum.photos/350/250?image=444" alt="" />
+      <div className={styles.transparentbox}>
+        <div className={styles.caption}>
+          <p>Library</p>
+          <p className={styles.opacitylow}>Architect Design</p>
+        </div>
+      </div> 
+    </div><div className={styles.imgbox}>
+      <img src="https://picsum.photos/350/250?image=444" alt="" />
+      <div className={styles.transparentbox}>
+        <div className={styles.caption}>
+          <p>Library</p>
+          <p className={styles.opacitylow}>Architect Design</p>
+        </div>
+      </div> 
+    </div><div className={styles.imgbox}>
+      <img src="https://picsum.photos/350/250?image=444" alt="" />
+      <div className={styles.transparentbox}>
+        <div className={styles.caption}>
+          <p>Library</p>
+          <p className={styles.opacitylow}>Architect Design</p>
+        </div>
+      </div> 
+    </div><div className={styles.imgbox}>
+      <img src="https://picsum.photos/350/250?image=444" alt="" />
+      <div className={styles.transparentbox}>
+        <div className={styles.caption}>
+          <p>Library</p>
+          <p className={styles.opacitylow}>Architect Design</p>
+        </div>
+      </div> 
+    </div><div className={styles.imgbox}>
+      <img src="https://picsum.photos/350/250?image=444" alt="" />
+      <div className={styles.transparentbox}>
+        <div className={styles.caption}>
+          <p>Library</p>
+          <p className={styles.opacitylow}>Architect Design</p>
+        </div>
+      </div> 
+    </div><div className={styles.imgbox}>
+      <img src="https://picsum.photos/350/250?image=444" alt="" />
+      <div className={styles.transparentbox}>
+        <div className={styles.caption}>
+          <p>Library</p>
+          <p className={styles.opacitylow}>Architect Design</p>
+        </div>
+      </div> 
+    </div><div className={styles.imgbox}>
+      <img src="https://picsum.photos/350/250?image=444" alt="" />
+      <div className={styles.transparentbox}>
+        <div className={styles.caption}>
+          <p>Library</p>
+          <p className={styles.opacitylow}>Architect Design</p>
+        </div>
+      </div> 
+    </div><div className={styles.imgbox}>
+      <img src="https://picsum.photos/350/250?image=444" alt="" />
+      <div className={styles.transparentbox}>
+        <div className={styles.caption}>
+          <p>Library</p>
+          <p className={styles.opacitylow}>Architect Design</p>
+        </div>
+      </div> 
+    </div> 
+    <div className={styles.imgbox}>
+      <img src="https://picsum.photos/350/250?image=444" alt="" />
+      <div className={styles.transparentbox}>
+        <div className={styles.caption}>
+          <p>Library</p>
+          <p className={styles.opacitylow}>Architect Design</p>
+        </div>
+      </div> 
+     </div>{/* //Here to add new images////////////////////////////////
+     <div className={styles.imgbox}> 
+      <img src="https://picsum.photos/350/250?image=444" alt="" />
+      <div className={styles.transparentbox}>
+        <div className={styles.caption}>
+          <p>Library</p>
+          <p className={styles.opacitylow}>Architect Design</p>
+        </div>
+      </div>  
+    </div>//////////////////*/}
+          {/* <div className={styles.containerimg}>
+            <div className={styles.gallery}>
+              <div className={classnames(styles.imggrid1, styles.singlecolumn)}><img className={styles.galleryimg} src="img/KastelSucuracCroatia.jpg" alt="Kastel Sucurac Croatia" /><p className={styles.gridText}>Kastel Sucurac Croatia</p></div>
+              <div className={classnames(styles.imggrid2, styles.singlecolumn)}><img className={styles.galleryimg} src="img/MilnaMarina.jpg" alt="MilnaMarina" /><p className={styles.gridText}>Milna Marina</p></div>
+              <div className={styles.imggrid3}><img className={styles.galleryimg} src="img/MilnaCroatia.jpeg" alt="MilnaCroatia.jpeg" /><p className={styles.gridText}>Milna Croatia</p></div>
+              <div className={styles.imggrid4}><img className={styles.galleryimg} src="img/MaslinicaCroatia.jpg" alt="Maslinica Croatia.jpg" /><p className={styles.gridText}> Maslinica Croatia on a Mooring Ball</p></div>
+              <div className={styles.imggrid5}><img className={styles.galleryimg} src="img/StomorskaCroatia.jpg" alt="Stomorska Croatia.jpg" /><p className={styles.gridText}>Stomorska Croatia</p></div>
+              <div className={styles.imggrid6}><img className={styles.galleryimg} src="img/KastelGomilicaCroatia.jpg" alt="Kastel Gomilica Croatia jpg" /><p className={styles.gridText}>Kastel Gomilica Beach Croatia</p></div>
+              
+            </div>
+            
+          </div> 
+         */}
+        </div>
+       
       </div>
-      
+      </div>
       </Layout>
     );
 }
